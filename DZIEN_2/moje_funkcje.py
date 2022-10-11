ebef635958e5cef20a34eb4d4ddbd79863f14a61
albim72 <marcin.albiniak72@gmail.com>
2022-10-11 12:31:43 +0200

    Update moje_funkcje.py

--- DZIEN_2/moje_funkcje.py
+++ DZIEN_2/moje_funkcje.py
@@ -1,3 +1,18 @@
+#pierwsza funkcja
+def witaj():
+    print("witaj nowy użytkowniku")
+    print("opłać abonament")
+    print("zaloguj się")
+
+witaj()
+witaj()
+
+for i in range(0,25,3):
+    print(f"_________wywołanie nr {i+1}_________")
+    witaj()
+else:
+    print("koniec")
+
 #druga funkcja
 
 def obywatel(nrtelefonu,kraj="Polska"):
@@ -9,3 +24,18 @@ obywatel(3425577677,"Grecja")
 obywatel(6897876565,"Brazylia")
 obywatel(7567786747)
 obywatel(None)
+
+
+#trzecia funkcja
+
+def gx(n):
+    return n**5
+
+f = 100
+def policz(a,b,x):
+    global f
+    f = (a+b)*x + f + gx(b)
+    return f
+
+print(policz(3,6,2))
+print(f)
