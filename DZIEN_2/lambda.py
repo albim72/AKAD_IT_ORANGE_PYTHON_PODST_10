ffd1518fbbcfb3781c74dcfd1ec755e08d6b18f3
albim72 <marcin.albiniak72@gmail.com>
2022-10-11 13:44:07 +0200

    Update lambda.py

--- DZIEN_2/lambda.py
+++ DZIEN_2/lambda.py
@@ -19,3 +19,13 @@ print(x(6))
 y = lambda a,b,c=2:(a-b)*c
 print(y(1,4,7))
 print(y(1,4))
+
+def multi(n):
+    return lambda a:a*n
+
+d = multi(56)
+print(d(7))
+
+print(multi(34)(19))
+
+liczby = [3,13,-1,0,4,15,16,22,23,101,99,120,-900,40,45]
