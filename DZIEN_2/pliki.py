1baaa7d11b1d25d30422bae7fb6b88fac04bfd54
albim72 <marcin.albiniak72@gmail.com>
2022-10-11 14:20:23 +0200

    Update pliki.py

--- DZIEN_2/pliki.py
+++ DZIEN_2/pliki.py
@@ -16,3 +16,27 @@ for x in f:
     print(x)
 f.close()
 
+g = open("message.txt","a",encoding="utf-8")
+g.write("to jest pierwsza linia pliku\n")
+g.close()
+
+f = open("message.txt","r",encoding="utf-8")
+print(f.read())
+f.close()
+
+if os.path.exists("message.txt"):
+    os.remove("message.txt")
+    print("plik został usunięty")
+else:
+    print("plik nie istnieje!")
+
+print("_______________________________________________")
+f = open("c:\\Temp\\result.txt","r",encoding="utf-8")
+print(f.read())
+f.close()
+
+if os.path.exists("c:\\Temp\\result.txt"):
+    os.remove("c:\\Temp\\result.txt")
+    print("plik został usunięty")
+else:
+    print("plik nie istnieje!")
