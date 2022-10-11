135b399c230f652ffbe195878c545f353e1d0f14
albim72 <marcin.albiniak72@gmail.com>
2022-10-11 11:48:44 +0200

    Update strings.py

--- DZIEN_2/strings.py
+++ DZIEN_2/strings.py
@@ -3,4 +3,26 @@ mecz = "mecz miesiąca:\nKlub sportowy: \"Orły Wisła\" - \tTrener: Jan Kot\nlo
 
 print(mecz)
 
+
 str_ = "       niezykle ważna i Tajna wiadmość;      RT5434556445     ;    i Tajna PrzEsYłKA"
+
+print(str_.upper())
+print(str_.lower())
+print(str_.strip())
+print(str_.replace("Tajna","Utajniona"))
+pd = str_.split(";")
+print(pd)
+
+for i,w in enumerate(pd):
+       pd[i] = w.strip()
+
+print(pd)
+print(str_.find("Tajna"))
+print(str_.endswith("ka"))
+print(str_.endswith("KA"))
+
+d = "pionierzy"
+e = "32455"
+
+print(d.isalpha())
+print(e.isdigit())
