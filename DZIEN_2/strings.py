27527dc2b887fd682632f1fb3ca5a8a71d7c30b3
albim72 <marcin.albiniak72@gmail.com>
2022-10-11 11:34:57 +0200

    Create strings.py

---
+++ DZIEN_2/strings.py
@@ -0,0 +1,4 @@
+mecz = "mecz miesiąca:\nKlub sportowy: \"Orły Wisła\" - \tTrener: Jan Kot\nlokalizacja: Janowiec\nvs\n" \
+       "Klub sportowy: \"KS Siłacz\" - \tTrener: Adam Nowak\nlokalizacja: Siłacze\n"
+
+print(mecz)
