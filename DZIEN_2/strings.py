d90c9b40c5463d5881cb0091a09529582f185830
albim72 <marcin.albiniak72@gmail.com>
2022-10-11 11:38:08 +0200

    Update strings.py

--- DZIEN_2/strings.py
+++ DZIEN_2/strings.py
@@ -2,3 +2,5 @@ mecz = "mecz miesiąca:\nKlub sportowy: \"Orły Wisła\" - \tTrener: Jan Kot\nlo
        "Klub sportowy: \"KS Siłacz\" - \tTrener: Adam Nowak\nlokalizacja: Siłacze\n"
 
 print(mecz)
+
+str_ = "       niezykle ważna i Tajna wiadmość;      RT5434556445     ;    i Tajna PrzEsYłKA"
