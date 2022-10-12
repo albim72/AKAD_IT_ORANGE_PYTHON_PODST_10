500b43e7dc001ffe3b0095e65e28baca1b6be742
albim72 <marcin.albiniak72@gmail.com>
2022-10-12 08:39:48 +0200

    Update osoby.py

--- DZIEN_3/osoby.py
+++ DZIEN_3/osoby.py
@@ -1,26 +1,29 @@
 class Osoba:
-    
+
     def __init__(self,imie,wiek,waga,wzrost):
         self.imie = imie
         self.wiek = wiek
         self.waga = waga
         self.wzrost = wzrost
+        self.kolor_oczu = "brązowe"
         self.info()
-        
-        
+
+
     def info(self):
         print("tworzenie nowej osoby jako obiektu superklasy Osoba...")
-        
-        
+
+
     def print_osoba(self):
-        print(f"osoba -> imię: {self.imie}, wiek: {self.wiek} lat(a), waga: {self.waga} kg, wzrost: {self.wzrost} cm.")
-        
-    
+        print(f"osoba -> imię: {self.imie}, wiek: {self.wiek} lat(a), waga: {self.waga} kg, "
+              f"wzrost: {self.wzrost} cm, kolor oczu: {self.kolor_oczu}.")
+
+
     def wiekza10lat(self):
         return self.wiek + 10
-    
+
     def czypracownik(self):
         return False
+
 
 os1 = Osoba("Jan",38,89,174)
 os1.print_osoba()
@@ -28,3 +31,6 @@ print(f"wiek za 10 lat: {os1.wiekza10lat()}")
 print(f"czy osoba jest pracownikiem? ({os1.czypracownik()})")
 
 print("________________________________________________________")
+
+
+
