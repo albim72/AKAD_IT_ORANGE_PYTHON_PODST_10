fbec386c0e459b1abf05b512a6a64df1060bb654
albim72 <marcin.albiniak72@gmail.com>
2022-10-12 08:36:35 +0200

    Update osoby.py

--- DZIEN_3/osoby.py
+++ DZIEN_3/osoby.py
@@ -21,3 +21,10 @@ class Osoba:
     
     def czypracownik(self):
         return False
+
+os1 = Osoba("Jan",38,89,174)
+os1.print_osoba()
+print(f"wiek za 10 lat: {os1.wiekza10lat()}")
+print(f"czy osoba jest pracownikiem? ({os1.czypracownik()})")
+
+print("________________________________________________________")
