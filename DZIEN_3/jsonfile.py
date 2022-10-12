138413e013f0c15bfb19eaf6f44a7b3bdbbc6931
albim72 <marcin.albiniak72@gmail.com>
2022-10-12 13:12:53 +0200

    Update jsonfile.py

--- DZIEN_3/jsonfile.py
+++ DZIEN_3/jsonfile.py
@@ -21,3 +21,23 @@ print(jsonsam)
 
 with open("sam.json","w",encoding="utf=8") as f:
     f.write(jsonsam)
+
+#do pliku pracownik dodaj nowego pracownika do źródła json -> jako nowy element listy pracowników
+
+def dodaj_nowego_pracownika(new_data,filename="pracownik.json"):
+
+    with open(filename,"r+",encoding="utf-8") as file:
+        file_data = json.load(file)
+        file_data["pra_info"].append(new_data)
+        file.seek(0)
+        json.dump(file_data,file,indent=4)
+
+nowy_pracownik = {
+            "imie": "Tekla",
+            "nazwisko": "Nowak",
+            "stanowisko": "prezes",
+            "lata_pracy": 12,
+            "email": "prezes@firma.pl"
+        }
+
+dodaj_nowego_pracownika(nowy_pracownik)
